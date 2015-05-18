b0746f066f732aaa6dca4d71cb97b95f67d41ba9
Daniel Law <djl1005@rit.edu>
2015-05-18 17:26:32 -0400

    Ask and thou shalt receive.
    they work now

--- StarFax/DirectX11_Starter/Shaders/ParticleGeometryShader.hlsl
+++ StarFax/DirectX11_Starter/Shaders/ParticleGeometryShader.hlsl
@@ -23,7 +23,7 @@ struct GStoPS
 };
 
 
-[maxvertexcount(6)]
+[maxvertexcount(4)]
 void main(point VStoGS input[1], inout TriangleStream<GStoPS> outStream)
 {
 	// Don't draw root particle itself
@@ -33,20 +33,26 @@ void main(point VStoGS input[1], inout TriangleStream<GStoPS> outStream)
 	GStoPS output;
 
 	// Offsets for smaller triangles
-	float4 offsets[6];
+	float4 offsets[4];
 	offsets[0] = float4(-0.1f, +0.1f, -0.1f, 0);
 	offsets[1] = float4(+0.1f, +0.1f, -0.1f, 0);
 	offsets[2] = float4(-0.1f, -0.1f, -0.1f, 0);
-	offsets[3] = float4(0.1f, +0.1f, -0.1f, 0);
-	offsets[4] = float4(+0.1f, -0.1f, -0.1f, 0);
-	offsets[5] = float4(-0.1f, -0.1f, -0.1f, 0);
+	offsets[3] = float4(+0.1f, -0.1f, -0.1f, 0);
+
+	float2 UVs[4];
+	UVs[0] = float2(0, 1);
+	UVs[1] = float2(1, 1);
+	UVs[2] = float2(0, 0);
+	UVs[3] = float2(1, 0);
+
+
 
 	// Calculate world view proj matrix
 	matrix wvp = mul(mul(world, view), projection);
 
 	// For each vert, spit out a small triangle
 	[unroll]
-	for (int o = 0; o < 6; o++)
+	for (int o = 0; o < 4; o++)
 	{
 		// Create a single vertex and add to the stream
 		output.position = mul(float4(input[0].position, 1.0f), wvp);
@@ -58,8 +64,7 @@ void main(point VStoGS input[1], inout TriangleStream<GStoPS> outStream)
 		output.position.xy += offsets[o].xy * depthChange * input[0].size;
 		output.color = input[0].color;
 
-		output.UV.x = (offsets[0].x / 2) + 0.05f * 10;
-		output.UV.y = (offsets[0].y / 2) + 0.05f * 10;
+		output.UV = UVs[o];
 
 		// Done
 		outStream.Append(output);
